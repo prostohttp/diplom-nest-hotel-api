8e5370edc16e739f2d7f872fc1e73afdadcd241d
Morozov Mikhail <memphis.sail@gmail.com>
2024-03-17 22:59:06 +1000

    FIX: UpdateHotelRoomParamsDto IsValidMongoId

--- src/hotel/dto/update-hotel-room-params.dto.ts
+++ src/hotel/dto/update-hotel-room-params.dto.ts
@@ -1,8 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { AddHotelRoomParamsDto } from "./add-hotel-room-params.dto";
+import { IsValidMongoId } from "src/validators/is-valid-mongo-id.validator";
 
 export class UpdateHotelRoomParamsDto extends AddHotelRoomParamsDto {
   @ApiProperty({ type: String, default: "65db5daa2f65467396335835" })
+  @IsValidMongoId()
   hotelId: string;
 
   @ApiProperty({ type: String, default: "Some description 1" })
